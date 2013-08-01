ce86c4257be74f656b12d0fbf04c59b3b6148cb4
Matthew York <myork@cs.ua.edu>
2013-08-01 09:34:16 -0500

    - Inital Mac Demo commit

---
+++ Demos/ObjectIO-Mac/ObjectIO/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ObjectIO
+//
+//  Created by Matt York on 7/12/13.
+//  Copyright (c) 2013 Matt York. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
